fa76637bcac74e24eac2d9d32bd73ad1ee33b87c
ftolentino <filmer.tolentino@gmail.com>
2021-05-17 18:02:35 -0700

    added HomePage css file to create a media query for 1440p desktop screen size

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import HeroSection from '../HeroSection/HeroSection';
 import {homeObjMain} from '../HomePage/Data';
+import './HomePage.css';
 
 const HomePage = () => {
   return (
-    <>
+    <div className="homepage">
       <HeroSection {...homeObjMain} />
-    </>
+    </div>
   )
 }
 
