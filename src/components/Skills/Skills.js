9360ecf575f15bc17c8671d80a248c267eee0e41
ftolentino <filmer.tolentino@gmail.com>
2021-10-06 18:44:29 -0700

    added flex-grid for icon container. styled each icon with their logo color.

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import '../Skills/Skills.css';
 import { 
-  SiJavascript, 
-  SiHtml5, 
-  SiCsswizardry, 
+  SiJavascript,
   SiReact, 
   SiGithub ,
   SiSass,
@@ -11,26 +9,56 @@ import {
   SiVisualstudiocode,
   SiAdobephotoshop
 } from 'react-icons/si';
-import { DiNodejs, DiCodeBadge } from 'react-icons/di';
+import { DiNodejs, DiCodeBadge, DiCss3, DiHtml5 } from 'react-icons/di';
 
 const Skills = () => {
   return (
     <div className="skills-container">
       <div className="title-container">
         <DiCodeBadge className="icon-container" />
-        <h3>Skills</h3>
+        <p>Skills</p>
       </div>
       <div className="icon-container">
-        <SiJavascript className="skill-icon" />
-        <DiNodejs className="skill-icon" />
-        <SiHtml5 className="skill-icon" />
-        <SiCsswizardry className="skill-icon" />
-        <SiSass className="skill-icon" />
-        <SiReact className="skill-icon" />
-        <SiBootstrap className="skill-icon" />
-        <SiGithub className="skill-icon" />
-        <SiAdobephotoshop className="skill-icon" />
-        <SiVisualstudiocode className="skill-icon" />
+        <div className="skill-icon">
+          <SiJavascript className="skill-js" />
+          <p>Javascript</p>
+        </div>
+        <div className="skill-icon">
+          <DiHtml5 className="skill-html" />
+          <p>HTML5</p>
+        </div>
+        <div className="skill-icon">
+          <DiCss3 className="skill-css" />
+          <p>CSS</p>
+        </div>
+        <div className="skill-icon">
+          <SiSass className="skill-sass" />
+          <p>Sass</p>
+        </div>
+        <div className="skill-icon">
+          <SiReact className="skill-react" />
+          <p>React</p>
+        </div>
+        <div className="skill-icon">
+          <SiBootstrap className="skill-bootstrap" />
+          <p>Bootstrap</p>
+        </div>
+        <div className="skill-icon">
+          <SiGithub className="skill-github" />
+          <p>Github</p>
+        </div>
+        <div className="skill-icon">
+          <SiAdobephotoshop className="skill-ps" />
+          <p>Adobe Photoshop</p>
+        </div>
+        <div className="skill-icon">
+          <SiVisualstudiocode className="skill-vscode" />
+          <p>Visual Studio Code</p>
+        </div>
+        <div className="skill-icon">
+          <DiNodejs className="skill-nodejs" />
+          <p>NodeJS</p>
+        </div>
       </div>
     </div>
   );
