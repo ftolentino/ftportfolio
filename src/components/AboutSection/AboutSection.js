5715fe366e7cf9ba116f9c0e64f0fc1fe21e9b4c
ftolentino <filmer.tolentino@gmail.com>
2023-03-09 15:38:39 -0800

    Update About Me

--- src/components/AboutSection/AboutSection.js
+++ src/components/AboutSection/AboutSection.js
@@ -10,32 +10,25 @@ const AboutSection = ({ headline, img, alt }) => {
             <div className='about-col'>
               <div className='about__hero-text-wrapper'>
                 <h1 className='about-heading'>{headline}</h1>
-                <p>Hello, I'm Filmer! I am a self-taught Frontend Developer. My career started out in games as an artist, 
+                <p>Hello, I'm Filmer! I started my journey into programming going the self-taught route, learning mostly Frontend Web Development. I then decided to dive into a 6 month full time Bootcamp where I established a solid foundation in Full Stack Mobile and Web Development.
+                  <br /> 
+                  <br /> 
+                  My background comes from game development as a 3D artist, 
                   making 3D environments for players to explore. After some time, I wanted to learn something different and
-                  found interest in the idea of programming to train the other side of my brain. It began as a journey to develop my logical 
-                  problem solving skills, not just in coding, but in life itself. I find satisfaction in creativity,
-                  learning new things, and expanding my skill sets.
+                  found interest in the idea of programming to train the other side of my brain. It began as a journey to develop logical 
+                  problem solving skills, not just in coding, but in life itself as a way of thinking. I find satisfaction in creativity,
+                  learning new things, and expanding my skill sets remaining technology forward thinking. I thrive best working with others, collaborating to solve problems and building something great.
                 </p>
                 <br/>
                 <h3>Things of interest</h3>
                 <p>Things that interest me are soccer, music, and video games. Each hobby involves a bit of creativity, problem solving skills, and team work.
                   <br/>
                   <br/>
-                  I'm an avid soccer enthusiast. I've been following the English Premier League since 2001 and have been playing on teams since gradeschool. 
-                  Every weekend I look forward to watching soccer games in the mornings, often times on multiple screens to watch different teams at once. 
-                  It truly is "The Beautiful Game" to me, from the team work, to individual skill, and the chaos of the ball physics!
+                  I'm an avid soccer enthusiast. I love to follow the English Premier League and other European Football competitions. I have been playing on teams since gradeschool and currently play in a 6v6 small goal adult league.
                   <br/>
                   <br/>
-                  I'm a self-taught guitar/bass player and have been playing in bands for 15 years. It's something that
-                  I enjoy doing for fun and as a way to creatively express myself. Making music with friends has been a great creative space
-                  to communicate through instruments and collectively write songs together.
-                  <br/>
-                  <br/>
-                  Gaming has been a part of me since I was a kid, from Oregon Trail, to consoles, to PC gaming. I like to absorb interactive 
-                  graphics and explore gameplay. I typically play multiplayer games because I enjoy the team aspect of working together towards a shared goal.
-                  <br/>
-                  <br/>
-                  Last but not least, I'm a dog dad of two Alaskan Klee Kai. They're my every day companions, my buddies.
+                  I enjoy writing songs on guitar and enjoy playing with friends to make music together. It's something that
+                  I enjoy doing for fun and as a way to creatively express myself.
                 </p>
               </div>
             </div>
